436f3eb9a43263680186c5b2ac4c7416f92b64bf
24122020_PBDP_HCMUS <99968921+DuckFard@users.noreply.github.com>
2024-12-15 21:47:01 +0700

    Update RoomManager.cs
    AI's edit

--- Multiplayer/RoomManager.cs
+++ Multiplayer/RoomManager.cs
@@ -1,66 +1,31 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class RoomManager : MonoBehaviourPunCallbacks
+public class RoomManager : MonoBehaviourPunCallbacks 
 {
-    public static RoomManager instance;
-    public GameObject player;
-
-    [Space] public Transform spawnPoint;
-
-    [Space] public GameObject roomCam;
-
-    [Space] public GameObject nameUI;
-
-    [Space] public GameObject connectingUI;
-
-    private string nickname = "unnamed";
-
-    private void Awake()
-    {
-        instance = this;
-    }
-
-    public void ChangeNickName(string _name) {
-        nickname = _name;
-    }
-
-    public void JoinRoomButtonPressed() {
-        Debug.Log("Connecting...");
-
-        PhotonNetwork.ConnectUsingSettings();
-
-        nameUI.SetActive(false);
-        connectingUI.SetActive(true);
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        base.OnConnectedToMaster();
-
-        Debug.Log("Connected to Server");
-
-        PhotonNetwork.JoinLobby();
-    }
-
+    private const string ROOM_NAME = "test";
+    private readonly RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 };
+    
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
-
-        Debug.Log("We're in the lobby");
-
-        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
     }
-
+    
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-
-        Debug.Log("We're connected in a room");
-
         roomCam.SetActive(false);
-        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
-        _player.GetComponent<PlayerSetup>().IsLocalPlayer(nickname);
+        SpawnPlayer();
     }
-
+    
+    private void SpawnPlayer()
+    {
+        var playerInstance = PhotonNetwork.Instantiate(
+            player.name,
+            spawnPoint.position,
+            Quaternion.identity
+        );
+        playerInstance.GetComponent<PlayerSetup>().IsLocalPlayer(nickname);
+    }
 }
