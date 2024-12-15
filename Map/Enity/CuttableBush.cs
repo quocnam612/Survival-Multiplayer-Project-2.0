a3fcc585acfaf2e6bb0fc20bfe096e674a4e033e
24122020_PBDP_HCMUS <99968921+DuckFard@users.noreply.github.com>
2024-12-15 22:17:19 +0700

    Update CuttableBush.cs

--- Map/Enity/CuttableBush.cs
+++ Map/Enity/CuttableBush.cs
@@ -1,21 +1,23 @@
 using Photon.Pun;
-using System.Collections;
-using System.IO;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements.Experimental;
 
 public class CuttableBush : MonoBehaviourPunCallbacks
 {
     public string prefabPath;
 
+    private InventorySystem inventorySystem;
+
+    private void Awake()
+    {
+        inventorySystem = FindObjectOfType<InventorySystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Hit range") && FindFirstObjectByType<InventorySystem>().handHold.transform.childCount != 0)
+        if (other.CompareTag("Hit range") && inventorySystem.handHold.transform.childCount != 0)
         {
             PhotonNetwork.Instantiate(prefabPath, transform.position, transform.rotation);
             PhotonNetwork.Destroy(gameObject);
         }
     }
-
 }
