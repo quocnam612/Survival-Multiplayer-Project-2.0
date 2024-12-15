7fe4049db6c6ec7d22ee545a715ab52a9293e433
24122020_PBDP_HCMUS <99968921+DuckFard@users.noreply.github.com>
2024-12-15 21:17:38 +0700

    Update CraftingTableSlot.cs
    AI's edit

--- Menu/CraftingTableSlot.cs
+++ Menu/CraftingTableSlot.cs
@@ -1,43 +1,40 @@
 using System;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class CraftingTableSlot : MonoBehaviour
 {
     public float ingredientCraft;
-    public UnityEngine.UI.Image icon;
-    public UnityEngine.UI.Image progressBar;
+    public Image icon;
+    public Image progressBar;
     public InventorySystem inventorySystem;
     public CraftingSystem craftingSystem;
 
     public CraftingSystem.Recipe recipe;
     private bool craftable;
-    public int playerIngredient;
+    private Dictionary<string, int> ingredientDictionary;
+
+    private void Start() {
+        ingredientDictionary = new Dictionary<string, int>();
+        foreach (var ingredient in recipe.ingredients) {
+            ingredientDictionary[ingredient.name] = ingredient.count;
+        }
+    }
 
     private void Update() {
-        playerIngredient = 0;
+        int playerIngredient = 0;
 
-        foreach (var ingredient in recipe.ingredients) 
-        {
-            foreach (var item in inventorySystem.itemList)
-            {
-                if (item.name == ingredient.name)
-                {
-                    playerIngredient += Mathf.Min(item.count, ingredient.count);
-                }
+        foreach (var item in inventorySystem.itemList) {
+            if (ingredientDictionary.ContainsKey(item.name)) {
+                playerIngredient += Mathf.Min(item.count, ingredientDictionary[item.name]);
             }
         }
 
-        progressBar.fillAmount = playerIngredient / ingredientCraft;
-        if (progressBar.fillAmount < 1f)
-        {
+        progressBar.fillAmount = (float)playerIngredient / ingredientCraft;
+        if (progressBar.fillAmount < 1f) {
             icon.color = new Color32(255, 255, 255, 69);
             craftable = false;  
-        }
-        else
-        {
+        } else {
             icon.color = new Color32(255, 255, 255, 220);
             craftable = true;
         }
@@ -47,16 +44,15 @@ public class CraftingTableSlot : MonoBehaviour
         if (craftable && !inventorySystem.isFull) {
             GameObject slot = inventorySystem.getSlot(recipe.name, recipe.count);
             if (slot) {
-                foreach (var ingredient in recipe.ingredients)
-                {
+                foreach (var ingredient in recipe.ingredients) {
                     inventorySystem.RemoveFromInventory(ingredient.name, ingredient.count);
                 }
                 if (slot.transform.childCount == 0) {
-                    GameObject itemCrafted = (GameObject)Instantiate(Resources.Load("Item Slot/" + recipe.name), slot.transform.position, slot.transform.rotation, slot.transform);
+                    GameObject itemCrafted = Instantiate(Resources.Load("Item Slot/" + recipe.name), slot.transform.position, slot.transform.rotation, slot.transform) as GameObject;
                     itemCrafted.name = recipe.name;
-                }
-                else {
-                    slot.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = (Int16.Parse(slot.transform.GetChild(0).GetChild(0).GetComponent<Text>().text) + 1).ToString();
+                } else {
+                    var itemCountText = slot.transform.GetChild(0).GetChild(0).GetComponent<Text>();
+                    itemCountText.text = (int.Parse(itemCountText.text) + 1).ToString();
                 }
             }
         }
